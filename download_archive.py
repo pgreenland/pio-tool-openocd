d17bf6f6484cbed0a47bf793c5e957ce9c34bc74
Phil Greenland <phil@quantulum.co.uk>
2026-01-11 16:45:41 +0000

    Final tweaks

--- download_archive.py
+++ download_archive.py
@@ -13,19 +13,18 @@ systype = get_systype().replace("_", "-")
 # OpenOCD version
 open_ocd_version = "0.12.0-7"
 
-# Download and unpack OpenOCD archive
+# Prepare download URL and filename
 url = f"https://github.com/xpack-dev-tools/openocd-xpack/releases/download/v{open_ocd_version}/xpack-openocd-{open_ocd_version}-{systype}.tar.gz"
 filename = f"xpack-openocd-{open_ocd_version}.tar.gz"
 
-# Print info
+# Download the archive
 print(f"Downloading and unpacking OpenOCD {open_ocd_version} for {systype} from {url} ...")
-
 response = requests.get(url)
 response.raise_for_status()
-
 with open(filename, 'wb') as f:
     f.write(response.content)
 
+# Unpack the archive
 unpack_archive(filename, getcwd())
 for file in listdir(f"xpack-openocd-{open_ocd_version}"):
   try:
