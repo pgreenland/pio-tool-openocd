18e037e0d14c390dd2ad6b11b47563dffd34ea05
Phil Greenland <phil@quantulum.co.uk>
2026-01-11 16:30:53 +0000

    Add macOS support

--- download_archive.py
+++ download_archive.py
@@ -5,15 +5,23 @@ from shutil import unpack_archive, move, Error
 from os import getcwd, listdir
 from os.path import join
 
+from platformio.util import get_systype
+
+# Get host system type
+systype = get_systype().replace("_", "-")
+
+# OpenOCD version
+open_ocd_version = "0.12.0-7"
+
 url = (
   "https://github.com/xpack-dev-tools/openocd-xpack/releases/download/"
-  "v0.12.0-7/xpack-openocd-0.12.0-7-linux-arm64.tar.gz"
+  f"v{open_ocd_version}/xpack-openocd-{open_ocd_version}-{systype}.tar.gz"
 )
-filename = "xpack-openocd-0.12.0-7.tar.gz"
+filename = f"xpack-openocd-{open_ocd_version}.tar.gz"
 path, headers = urlretrieve(url, filename)
 unpack_archive(path, getcwd())
-for file in listdir("xpack-openocd-0.12.0-7"):
+for file in listdir(f"xpack-openocd-{open_ocd_version}"):
   try:
-    move(join("xpack-openocd-0.12.0-7", file), getcwd())
+    move(join(f"xpack-openocd-{open_ocd_version}", file), getcwd())
   except Error as err:
     print(err)
