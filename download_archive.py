92a2f875a7e5a6818be22b76a7b4d161fb0a600f
Phil Greenland <phil@quantulum.co.uk>
2026-01-11 16:43:27 +0000

    Attempt download with requests

--- download_archive.py
+++ download_archive.py
@@ -1,10 +1,10 @@
 #!/bin/env python
 
-from urllib.request import urlretrieve
 from shutil import unpack_archive, move, Error
 from os import getcwd, listdir
 from os.path import join
 
+import requests
 from platformio.util import get_systype
 
 # Get host system type
@@ -20,8 +20,13 @@ filename = f"xpack-openocd-{open_ocd_version}.tar.gz"
 # Print info
 print(f"Downloading and unpacking OpenOCD {open_ocd_version} for {systype} from {url} ...")
 
-path, headers = urlretrieve(url, filename)
-unpack_archive(path, getcwd())
+response = requests.get(url)
+response.raise_for_status()
+
+with open(filename, 'wb') as f:
+    f.write(response.content)
+
+unpack_archive(filename, getcwd())
 for file in listdir(f"xpack-openocd-{open_ocd_version}"):
   try:
     move(join(f"xpack-openocd-{open_ocd_version}", file), getcwd())
